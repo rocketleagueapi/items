efe1b85a494b514e2a3366f132053b6edfad3ac8
AngaBlue <contact@anga.blue>
2021-11-21 22:48:35 +1100

    Items

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as certs } from './parsed/certs.json';
+export { default as paints } from './parsed/paints.json';
+export { default as products } from './parsed/products.json';
+export { default as series } from './parsed/series.json';
+export { default as slots } from './parsed/slots.json';
+export { default as specials } from './parsed/specials.json';
---
+++ src/parse.ts
@@ -0,0 +1,55 @@
+import CertificationDump from './bakkesmod/CertificationDump.json';
+import PaintDump from './bakkesmod/PaintDump.json';
+import ProductDump from './bakkesmod/ProductDump.json';
+import SeriesDump from './bakkesmod/SeriesDump.json';
+import SlotDump from './bakkesmod/SlotDump.json';
+import SpecialEditionDump from './bakkesmod/SpecialEditionDump.json';
+import { writeFileSync } from 'fs';
+import path from 'path';
+
+function write(name: string, data: any) {
+    writeFileSync(path.join(__dirname, './parsed/', name + '.json'), JSON.stringify(data));
+}
+
+const certs = CertificationDump.reduce((acc, c) => {
+    acc[c['Certified Database Id']] = c['Certified Database Name'];
+    return acc;
+}, {} as Record<number, string>);
+write('certs', certs);
+
+const paints = PaintDump.reduce((acc, p) => {
+    acc[p['Paint Database Id']] = p['Paint Database Label'];
+    return acc;
+}, {} as Record<number, string>);
+write('paints', paints);
+
+const products = ProductDump.reduce((acc, p) => {
+    acc[p['Product Id']] = {
+        name: p['Product Long Label'],
+        blueprint: p['Product Blueprint'],
+        paintable: p['Product Paintable'],
+        currency: p['Product Currency'],
+        quality: p['Product Quality Id'],
+        slot: p['Slot Index']
+    }
+    return acc;
+}, {} as Record<number, any>);
+write('products', products);
+
+const series = SeriesDump.reduce((acc, s) => {
+    acc[s['Database Series Id']] = s['Database Series Label'];
+    return acc;
+}, {} as Record<number, string>);
+write('series', series);
+
+const slots = SlotDump.reduce((acc, s) => {
+    acc[s['Slot Index']] = s['Slot Label'] === 'None' ? s['Slot Label'] : s['Slot Online Label'];
+    return acc;
+}, {} as Record<number, string>);
+write('slots', slots);
+
+const specials = SpecialEditionDump.reduce((acc, s) => {
+    acc[s['Special Database Id']] = s['Special Database Label'];
+    return acc;
+}, {} as Record<number, string>);
+write('specials', specials);
