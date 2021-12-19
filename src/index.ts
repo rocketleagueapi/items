09a6bec19ed7611371fa4005d1b4234f9007cf2a
AngaBlue <contact@anga.blue>
2021-12-20 02:42:22 +1100

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export { default as certs } from './parsed/certs.json';
+export { default as maps } from './parsed/maps.json';
 export { default as paints } from './parsed/paints.json';
+export { default as playlists } from './parsed/playlists.json';
 export { default as products } from './parsed/products.json';
+export { default as qualities } from './parsed/qualities.json';
 export { default as series } from './parsed/series.json';
 export { default as slots } from './parsed/slots.json';
 export { default as specials } from './parsed/specials.json';
+export { default as titles } from './parsed/titles.json';
