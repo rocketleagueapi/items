5ec384329307e81b80fa115d79c365d37ac697eb
AngaBlue <contact@anga.blue>
2022-12-17 00:33:32 +1100

    Updated special overrides

--- src/specialMap.ts
+++ src/specialMap.ts
@@ -1,31 +1,48 @@
 const specialMap: Record<number, number> = {
-    4585: 3,
-    4347: 2,
-    4001: 1,
-    4748: 3,
-    5584: 1,
-    3900: 3,
-    4721: 2,
-    4326: 2,
-    4849: 1,
-    4000: 3,
-    4217: 3,
-    4371: 1,
-    4112: 5,
-    4294: 3,
-    3898: 1,
-    4690: 2,
-    3897: 2,
-    4203: 3,
-    4839: 3,
-    4652: 2,
-    4932: 3,
-    4222: 2,
-    3901: 2,
-    4287: 1,
-    3899: 2,
-    4370: 3,
-    3369: 3
+    // 2796 2661 The Flash Duplicate
+    3897: 2, // Sprocket
+    3898: 1, // Rocket Forge II
+    3899: 2, // Asik
+    3900: 3, // Gripstride HX
+    3901: 2, // Zowie
+    4000: 3, // Metalwork
+    4001: 1, // Galvan
+    4112: 5, // Polaris
+    // 4122 1713 Apex Team Edition
+    4128: 1, // Usurper
+    4203: 3, // Stella
+    4217: 3, // P-SIMM
+    4222: 2, // Zefram
+    4287: 1, // Draco
+    4294: 3, // Pyrrhos
+    4314: 3, // Automaton
+    4326: 2, // Irradiator
+    4347: 2, // DRN
+    4358: 3, // Apparatus
+    4370: 3, // Camo
+    4371: 1, // Picket
+    4426: 3, // Blade Wave
+    4585: 3, // Dimonix
+    4599: 2, // Hikari P5
+    4627: 3, // Saptarishi
+    4652: 2, // Yankii RL
+    4690: 2, // Shima
+    4712: 2, // Cristiano
+    4721: 2, // IO
+    4748: 3, // Glaive
+    4777: 2, // Cristiano
+    // 4778 388 Spinner Duplicate
+    4839: 3, // Tremor
+    4849: 1, // Jandertek
+    4932: 3 // Z-RO
+    // 6116 3310 Zefram Duplicate
+    // 6204 2606 Fast & Furious Dodge Charger Duplicate
+    // 6205 6030 Fast & Furious Pontiac Fiero Duplicate
+    // 6206 2472 Fast & Furious Nissan Skyline Duplicate
+    // 6207 2955 Jurassic Jeep® Wrangler Duplicate
+    // 6208 4134 K.I.T.T. Duplicate
+    // 6209 609 DeLorean Time Machine Duplicate
+    // 6988 5336 Pirelli Duplicate
 };
 
 export default specialMap;
