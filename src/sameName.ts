471c272c77628245d217e8fcbf35e232f07858e3
AngaBlue <contact@anga.blue>
2023-01-19 13:58:55 +1100

    Update sameName script

--- src/sameName.ts
+++ src/sameName.ts
@@ -6,7 +6,11 @@ for (const [id, product] of Object.entries(products)) {
     if (product.slot !== 2 && product.slot !== 1) continue;
     const duplicate = entries.find(
         ([pid, p]) =>
-            p.name === product.name && pid < id && p.special === product.special && p.slot === 2 && !duplicates.includes(Number(pid))
+            p.name === product.name &&
+            pid < id &&
+            p.special === product.special &&
+            p.slot === product.slot &&
+            !duplicates.includes(Number(pid))
     );
     if (duplicate) {
         console.log(`Duplicate name: ${product.name} (${duplicate[0]}) (${id})`);
